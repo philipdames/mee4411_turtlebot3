1ea45a63797705271ef03925a76322868f9fe1f1
Philip Dames <pdames@temple.edu>
2024-01-14 21:20:31 -0500

    Update remote_pc_setup_noetic.sh
    Update install script

--- remote_pc_setup_noetic.sh
+++ remote_pc_setup_noetic.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Print status message in white text on blue background
+print_status () {
+  echo "\033[37;44m$1\033[0m"
+}
+
+# Function to add if no such line in file
+ainsl () {
+  grep -qF -- "$1" $2 || echo "$1" >> $2
+}
+
 # Print message giving an overview of the installation process
 echo ""
 echo "[Note] Target OS version  >>> Ubuntu 20.04.x (Focal Fossa)"
@@ -8,34 +18,34 @@ echo "[Note] Catkin workspace   >>> $HOME/mee4411_ws"
 echo ""
 echo "PRESS [ENTER] TO CONTINUE THE INSTALLATION"
 echo "IF YOU WANT TO CANCEL, PRESS [CTRL] + [C]"
-read
+read REPLY
 
-echo "[Set the target OS, ROS version and name of catkin workspace]"
+print_status "[Set the target OS, ROS version and name of catkin workspace]"
 name_os_version=${name_os_version:="focal"}
 name_ros_version=${name_ros_version:="noetic"}
 name_catkin_workspace=${name_catkin_workspace:="mee4411_ws"}
 
 # Make sure all of the software in the Ubuntu installation is up to date before starting
-echo "[Update the package lists and upgrade them]"
+print_status "[Update the package lists and upgrade them]"
 sudo apt update -y
 sudo apt upgrade -y
 
-echo "[Install build environment, the chrony, ntpdate and set the ntpdate]"
+print_status "[Install build environment, the chrony, ntpdate and set the ntpdate]"
 sudo apt install -y chrony ntpdate curl build-essential
 sudo ntpdate ntp.ubuntu.com
 
-echo "[Add the ROS repository]"
+print_status "[Add the ROS repository]"
 if [ ! -e /etc/apt/sources.list.d/ros-latest.list ]; then
   sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 fi
 
-echo "[Download the ROS keys]"
+print_status "[Download the ROS keys]"
 roskey=`apt-key list | grep "Open Robotics"`
 if [ -z "$roskey" ]; then
   curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 fi
 
-echo "[Check the ROS keys]"
+print_status "[Check the ROS keys]"
 roskey=`apt-key list | grep "Open Robotics"`
 if [ -n "$roskey" ]; then
   echo "[ROS key exists in the list]"
@@ -44,65 +54,78 @@ else
   exit 0
 fi
 
-echo "[Update the package lists]"
+print_status "[Update the package lists]"
 sudo apt update -y
 
-echo "[Install the ros-desktop-full and all rqt plugins]"
+print_status "[Install the ros-desktop-full and all rqt plugins]"
 sudo apt install -y ros-$name_ros_version-desktop-full ros-$name_ros_version-rqt-*
 
-echo "[Install RQT & Gazebo]"
+print_status "[Install RQT & Gazebo]"
 sudo apt install -y ros-$name_ros_version-rqt-* ros-$name_ros_version-gazebo-*
 
-echo "[Install other ROS packages]"
-sudo apt install -y ros-noetic-joy ros-noetic-teleop-twist-joy \
-  ros-noetic-teleop-twist-keyboard ros-noetic-laser-proc \
-  ros-noetic-rgbd-launch ros-noetic-rosserial-arduino \
-  ros-noetic-rosserial-python ros-noetic-rosserial-client \
-  ros-noetic-rosserial-msgs ros-noetic-amcl ros-noetic-map-server \
-  ros-noetic-move-base ros-noetic-urdf ros-noetic-xacro \
-  ros-noetic-compressed-image-transport ros-noetic-rqt* ros-noetic-rviz \
-  ros-noetic-gmapping ros-noetic-navigation ros-noetic-interactive-markers
+print_status "[Install other ROS packages]"
+sudo apt install -y ros-noetic-joy \
+  ros-noetic-teleop-twist-joy \
+  ros-noetic-teleop-twist-keyboard \
+  ros-noetic-laser-proc \
+  ros-noetic-rgbd-launch \
+  ros-noetic-rosserial-arduino \
+  ros-noetic-rosserial-python \
+  ros-noetic-rosserial-client \
+  ros-noetic-rosserial-msgs \
+  ros-noetic-amcl \
+  ros-noetic-map-server \
+  ros-noetic-move-base \
+  ros-noetic-urdf \
+  ros-noetic-xacro \
+  ros-noetic-compressed-image-transport \
+  ros-noetic-rqt* \
+  ros-noetic-rviz \
+  ros-noetic-gmapping \
+  ros-noetic-navigation \
+  ros-noetic-interactive-markers
 
-echo "[Install Turtlebot3 ROS packages]"
+print_status "[Install Turtlebot3 ROS packages]"
 sudo apt install -y ros-noetic-dynamixel-sdk ros-noetic-turtlebot3-msgs ros-noetic-turtlebot3
 
-echo "[Install catkin_tools]"
+print_status "[Install catkin_tools]"
 sudo apt install -y python3-catkin-tools
 
-echo "[Environment setup and getting rosinstall]"
+print_status "[Environment setup and getting rosinstall]"
 source /opt/ros/$name_ros_version/setup.sh
 sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
 
-echo "[Install rosdep and Update]"
+print_status "[Install rosdep]"
 sudo apt install python3-rosdep
 
-echo "[Initialize rosdep and Update]"
-sudo sh -c "rosdep init"
-rosdep update
+if ! [ -f /etc/ros/rosdep/sources.list.d/20-default.list ]; then
+  print_status "[Initialize rosdep and Update]"
+  sudo sh -c "rosdep init"
+  rosdep update
+fi
 
-echo "[Make the catkin workspace and test the catkin_make]"
-mkdir -p $HOME/$name_catkin_workspace/src
-cd $HOME/$name_catkin_workspace/src
-catkin_init_workspace
-cd $HOME/$name_catkin_workspace
-catkin_make
+if ! [ -d $HOME/$name_catkin_workspace ]; then
+  print_status "[Make the catkin workspace and test the catkin_make]"
+  mkdir -p $HOME/$name_catkin_workspace/src
+  cd $HOME/$name_catkin_workspace
+  catkin init
+  catkin build
+fi
 
-echo "[Set the ROS evironment]"
-sh -c "echo \"alias eb='nano ~/.bashrc'\" >> ~/.bashrc"
-sh -c "echo \"alias sb='source ~/.bashrc'\" >> ~/.bashrc"
-sh -c "echo \"alias gs='git status'\" >> ~/.bashrc"
-sh -c "echo \"alias gp='git pull'\" >> ~/.bashrc"
-sh -c "echo \"alias cw='cd ~/$name_catkin_workspace'\" >> ~/.bashrc"
-sh -c "echo \"alias cs='cd ~/$name_catkin_workspace/src'\" >> ~/.bashrc"
-sh -c "echo \"alias cm='cd ~/$name_catkin_workspace && catkin_make'\" >> ~/.bashrc"
+print_status "[Set the ROS evironment]"
+ainsl "alias eb='nano ~/.bashrc'" ~/.bashrc
+ainsl "alias sb='source ~/.bashrc'" ~/.bashrc
+ainsl "alias gs='git status'" ~/.bashrc
+ainsl "alias gp='git pull'" ~/.bashrc
+ainsl "alias cw='cd ~/$name_catkin_workspace" ~/.bashrc
+ainsl "alias cs='cd ~/$name_catkin_workspace/src'" ~/.bashrc
+ainsl "alias cb='cd ~/$name_catkin_workspace && catkin build'" ~/.bashrc
 
-sh -c "echo \"source /opt/ros/$name_ros_version/setup.bash\" >> ~/.bashrc"
-sh -c "echo \"source ~/$name_catkin_workspace/devel/setup.bash\" >> ~/.bashrc"
+ainsl "source /opt/ros/$name_ros_version/setup.bash" ~/.bashrc
+ainsl "source ~/$name_catkin_workspace/devel/setup.bash" ~/.bashrc
 
-sh -c "echo \"export ROS_MASTER_URI=http://localhost:11311\" >> ~/.bashrc"
-sh -c "echo \"export ROS_HOSTNAME=localhost\" >> ~/.bashrc"
+ainsl "export ROS_MASTER_URI=http://localhost:11311" ~/.bashrc
+ainsl "export ROS_HOSTNAME=localhost" ~/.bashrc
 
-source $HOME/.bashrc
-
-echo "[Complete!!!]"
+print_status "[Complete!!!]"
 exit 0
