9fd620fae54fa175da4a00e4cfcee0489d9ef4c1
Philip Dames <pdames@temple.edu>
2024-01-27 15:37:22 -0500

    Update launch files

--- mee4411_core/scripts/laser_repub.py
+++ mee4411_core/scripts/laser_repub.py
@@ -1,28 +1,22 @@
-#!/usr/bin/env python  
+#!/usr/bin/env python3 
 import rospy
 from sensor_msgs.msg import LaserScan
 
-pub = []
-frame_id = ""
+class LaserRepub:
+    def __init__(self) -> None:
+        self.frame_id = rospy.get_param("frame_id", "base_scan")
+        self.pub = rospy.Publisher('~scan_out', LaserScan, queue_size=10)
+        rospy.sleep(0.1)
+        self.sub = rospy.Subscriber('scan', LaserScan, self.callback)
 
-def callback(msg):
-    msg.header.frame_id = frame_id
-    pub.publish(msg)
+    def callback(self, msg):
+        msg.header.frame_id = self.frame_id
+        self.pub.publish(msg)
 
 def scan_repub():
     rospy.init_node('scan_repub')
-    
-    global frame_id
-    frame_id = rospy.get_param("frame_id", "base_scan")
-    
-    global pub
-    pub = rospy.Publisher('~scan_out', LaserScan, queue_size=10)
-    
-    rospy.sleep(0.1)
-    rospy.Subscriber('scan', LaserScan, callback)
-    
+    lr = LaserRepub()
     rospy.spin()
-
 
 if __name__ == '__main__':
     try:
