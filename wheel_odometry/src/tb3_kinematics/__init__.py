b2200d4881009e0adbdb05c5d72ee6e0ce3173c4
Philip Dames <pdames@temple.edu>
2024-05-13 15:02:49 -0400

    Update wheel odometry package

---
+++ wheel_odometry/src/tb3_kinematics/__init__.py
@@ -0,0 +1,84 @@
+from sensor_msgs.msg import JointState
+
+import numpy as np
+from typing import List
+
+# Indexing values
+LEFT = 0
+RIGHT = 1
+
+X = 0
+Y = 1
+THETA = 2
+
+class TB3Kinematics:
+    def __init__(self, robot_model) -> None:
+        if robot_model == 'burger':
+            self.wheel_separation = 0.160 # [m]
+            self.turning_radius = 0.080 # [m]
+            self.robot_radius = 0.105 # [m]
+        elif robot_model == 'waffle' or robot_model == 'waffle_pi':
+            self.wheel_separation = 0.287 # [m]
+            self.turning_radius = 0.1435 # [m]
+            self.robot_radius = 0.220 # [m]
+        else:
+            raise Exception(f'Turtlebot3 model {robot_model} not defined')
+        self.wheel_radius = 0.033 # [m]
+
+
+    def calculate_wheel_change(self, new_joint_states: JointState, prev_joint_states: JointState) -> List[float]:
+        # Inputs:
+        #   new_joint_states    new joint states
+        #   prev_joint_states   previous joint states
+        # Outputs:
+        #   delta_wheel_l       change in left wheel angle [rad]
+        #   delta_wheel_r       change in right wheel angle [rad]
+        #   delta_time          change in time [s]
+
+        ##### YOUR CODE STARTS HERE #####
+        # TODO Calculate the output values
+        delta_wheel_l = 0.
+        delta_wheel_r = 0.
+        delta_time    = 0.
+        ##### YOUR CODE ENDS HERE   #####
+        
+        # Data validation
+        if np.isnan(delta_wheel_l):
+            delta_wheel_l = 0.0
+        if np.isnan(delta_wheel_r):
+            delta_wheel_r = 0.0
+
+        return (delta_wheel_l, delta_wheel_r, delta_time)
+
+
+    def calculate_displacement(self, delta_wheel_l: float, delta_wheel_r: float) -> List[float]:
+        # Inputs:
+        #   delta_wheel_l       change in left wheel angle [rad]
+        #   delta_wheel_r       change in right wheel angle [rad]
+        # Outputs:
+        #   delta_s         linear displacement [m]
+        #   delta_theta     angular displacement [rad]
+
+        ##### YOUR CODE STARTS HERE #####
+        # TODO Calculate the output values
+        delta_s     = 0. # linear displacement [m]
+        delta_theta = 0. # angular displacement [rad]
+        ##### YOUR CODE ENDS HERE   #####
+
+        return (delta_s, delta_theta)
+
+
+    def calculate_pose(self, prev_pose: List[float], delta_s: float, delta_theta: float) -> List[float]:
+        # Inputs:
+        #   prev_pose       input pose in format (x, y, theta) [m, m, rad]
+        #   delta_s         linear displacement [m]
+        #   delta_theta     angular displacement [rad]
+        # Outputs:
+        #   pose            output pose in format (x, y, theta) [m, m, rad]
+
+        ##### YOUR CODE STARTS HERE #####
+        # TODO Calculate the output values
+        pose = prev_pose # FILL THIS IN
+        ##### YOUR CODE ENDS HERE   #####
+
+        return pose
--- wheel_odometry/tests/test_kinematics.py
+++ wheel_odometry/tests/test_kinematics.py
@@ -6,12 +6,14 @@ import numpy as np
 import rospy
 from sensor_msgs.msg import JointState
 
-import tb3_kinematics as kn
+from tb3_kinematics import TB3Kinematics
 
 ## A sample python unit test
 class TestKinematics(unittest.TestCase):
     ## test calculate_wheel_change
     def test_calculate_wheel_change(self): # only functions with 'test_'-prefix will be run!
+        kn = TB3Kinematics('burger')
+
         # Set up test inputs
         js_prev = JointState()
         js_prev.header.stamp = rospy.Time(0)
@@ -32,12 +34,14 @@ class TestKinematics(unittest.TestCase):
         data = kn.calculate_wheel_change(js_new, js_prev)
         
         # Ensure that calculated outputs match desired outputs
-        for t, d, n in zip(data_true, data, names):
-            self.assertEqual(t, d, "Data %s has the wrong value (%.3f instead of %.3f)" % (n, d, t))
-    
-    
+        for d_true, d, name in zip(data_true, data, names):
+            self.assertEqual(d_true, d, f"Data {name} has the wrong value ({d:.3f} instead of {d_true:.3f})")
+
+
     ## test calculate_displacement
     def test_calculate_displacement(self):
+        kn = TB3Kinematics('burger')
+
         # Set up test inputs
         delta_wheel_l = np.array([0.696, -0.447, 0.676, 0.38, -0.4])
         delta_wheel_r = np.array([0.325, 0.083, -0.711, 0.629, 0.378])
@@ -52,16 +56,20 @@ class TestKinematics(unittest.TestCase):
         delta_s = [None] * len(delta_wheel_l)
         delta_theta = [None] * len(delta_wheel_l)
         for i, (dwl, dwr, wr, ws) in enumerate(zip(delta_wheel_l, delta_wheel_r, wheel_radius, wheel_separation)):
-            (delta_s[i], delta_theta[i]) = kn.calculate_displacement(dwl, dwr, wr, ws)
+            kn.wheel_radius = wr
+            kn.wheel_separation = ws
+            (delta_s[i], delta_theta[i]) = kn.calculate_displacement(dwl, dwr)
         
         # Ensure that calculated outputs match desired outputs
         for ds, dt, ds_true, dt_true in zip(delta_s, delta_theta, delta_s_true, delta_theta_true):
-            self.assertTrue(np.abs(ds - ds_true) < 1e-6, "Linear displacement is wrong (%.6f instead of %.6f)" % (ds, ds_true))
-            self.assertTrue(np.abs(dt - dt_true) < 1e-6, "Angular displacement is wrong (%.6f instead of %.6f)" % (dt, dt_true))
-    
+            self.assertTrue(np.abs(ds - ds_true) < 1e-6, f"Linear displacement is wrong ({ds:.6f} instead of {ds_true:.6f})")
+            self.assertTrue(np.abs(dt - dt_true) < 1e-6, f"Angular displacement is wrong ({dt:.6f} instead of {dt_true:.6f})")
+
 
     ## test calculate_pose
     def test_calculate_pose(self):
+        kn = TB3Kinematics('burger')
+
         # Set up test inputs
         pose_in = [0.5, 0.3, -1.0]
         delta_s = 0.2
@@ -76,9 +84,4 @@ class TestKinematics(unittest.TestCase):
         # Ensure that calculated outputs match desired outputs
         names = ['x', 'y', 'theta']
         for p, p_true, n in zip(pose, pose_true, names):
-            self.assertTrue(np.abs(p - p_true) < 1e-6, "Pose %s is wrong (%.6f instead of %.6f)" % (n, p, p_true))
-
-
-if __name__ == '__main__':
-    import rostest
-    rostest.rosrun('wheel_odometry', 'test_kinematics', TestKinematics)
+            self.assertTrue(np.abs(p - p_true) < 1e-6, f"Pose {n} is wrong ({p:.6f} instead of {p_true:.6f})")
